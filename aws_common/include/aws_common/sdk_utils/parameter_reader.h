919d1756bffbb733a8b1937c3442b6fc94fde69e
AAlon <avishayalon@gmail.com>
2019-01-22 11:51:50 -0800

    Parameter Namespacing: Refactoring using the ParameterPath object. (#10)
    * Refactoring using the ParameterPath object.
    
    * Tweaking documentation
    
    * Removing implicit conversion of namespace separators and adjusting ClientConfigurationProvider accordingly.
    
    * Adding explicit guarantee in doc of parameter_reader.h that the arguments remain unchanged if the query was unsuccessful.
    
    * Adjusted client_configuration_provider tests for the new ParameterReader API accepting ParameterPath.
    
    * Changing ParameterPath's name_ member to be const.

--- aws_common/include/aws_common/sdk_utils/parameter_reader.h
+++ aws_common/include/aws_common/sdk_utils/parameter_reader.h
@@ -20,6 +20,110 @@
 namespace Aws {
 namespace Client {
 
+class ParameterPath {
+public:
+    /**
+     * Legacy constructor for backwards compatibility.
+     * @hint For ROS1, the node namespace separator is the same as the parameter namespace separator,
+     *     and so the path would simply be the given string (dictated by Ros1NodeParameterReader).
+     *   For ROS2 the separators differ; If only node namespace separators were found, we would treat them as parameter separators.
+     * @param name Parameter name, which in practice would be the path of the parameter, e.g. config/timeoutMs.
+     */
+    ParameterPath(const char * name) : name_(name) {}
+
+    /**
+     * @example The parameter under node "lidar_node" in namespace "sensor_nodes",
+     * with parameter namespace "settings" and parameter key "frequency", should be specified as follows:
+     *   node_namespace: ["sensor_nodes", "lidar_node"]
+     *   parameter_path_keys: ["settings", "frequency"]
+     * @note Node namespaces should be empty if used by a node looking for a parameter of its own (a local parameter).
+     * @param node_namespaces
+     * @param parameter_path_keys
+     */
+    ParameterPath(const std::vector<std::string> & node_namespaces, const std::vector<std::string> & parameter_path_keys) :
+      node_namespaces_(node_namespaces), parameter_path_keys_(parameter_path_keys) {}
+
+    /**
+     * @example The local parameter "timeout" in parameter namespace "config" should be specified as follows:
+     *   ParameterPath("config", "timeout")
+     * @tparam Args
+     * @param parameter_path_keys
+     */
+    template<typename ...Args>
+    ParameterPath(Args... parameter_path_keys) :
+      parameter_path_keys_(std::vector<std::string>{(parameter_path_keys)...}) {}
+
+    /**
+     * @note Only applies if node_namespaces was specified during construction; otherwise, an empty string is returned.
+     * @param node_namespace_separator
+     * @return string The parameter's whereabouts in the node namespace hierarchy.
+     */
+    std::string get_node_path(char node_namespace_separator) const
+    {
+        std::string resolved_path;
+        /* Construct the node's path by the provided lists of keys */
+        for (auto key = node_namespaces_.begin(); key != node_namespaces_.end(); key++) {
+            resolved_path += *key + node_namespace_separator;
+        }
+        if (!resolved_path.empty() && resolved_path.back() == node_namespace_separator) {
+            resolved_path.pop_back();
+        }
+        return resolved_path;
+    }
+    /**
+     * @note Only applies if parameter_path_keys was specified during construction; otherwise, an empty string is returned.
+     * @param parameter_namespace_separator
+     * @return string The parameter path including parameter namespaces but excluding node's namespaces.
+     */
+    std::string get_local_path(char parameter_namespace_separator) const
+    {
+        std::string resolved_path;
+        /* Construct the parameter's path by the provided lists of keys */
+        for (auto key = parameter_path_keys_.begin(); key != parameter_path_keys_.end(); key++) {
+            resolved_path += *key + parameter_namespace_separator;
+        }
+        if (!resolved_path.empty() && resolved_path.back() == parameter_namespace_separator) {
+            resolved_path.pop_back();
+        }
+        return resolved_path;
+    }
+    /**
+     * Resolves & returns the parameter's path. Supports two separate use cases:
+     *  1. A single, 'flat' string was provided upon construction.
+     *    In this case, we return it as is.
+     *  2. Detailed lists of strings were specified for the parameter & node namespaces.
+     *    In this case, we construct the resolved path using the provided separators.
+     * @param node_namespace_separator
+     * @param parameter_namespace_separator
+     * @return string representing the full, resolved path of the parameter.
+     * @note If node_namespaces and parameter_path_keys are empty, an empty string would be returned.
+     */
+    std::string get_resolved_path(char node_namespace_separator,
+                                  char parameter_namespace_separator) const
+    {
+      if (!name_.empty()) {
+        return name_;
+      } else {
+          std::string resolved_path = get_node_path(node_namespace_separator);
+          if (!resolved_path.empty()) {
+              resolved_path += node_namespace_separator;
+          }
+          resolved_path += get_local_path(parameter_namespace_separator);
+          return resolved_path;
+      }
+    }
+private:
+    /**
+     * Member variables to store the parameter's path in the namespace hierarchy.
+     * parameter_path_keys_ Parameter namespaces list.
+     * node_namespaces_ Node namespaces list.
+     * name_ A single string. Used when simpler initialization is required, instead of the aforementioned namespace lists.
+     */
+    const std::vector<std::string> parameter_path_keys_;
+    const std::vector<std::string> node_namespaces_;
+    const std::string name_;
+};
+
 class ParameterReaderInterface
 {
 public:
@@ -28,58 +132,65 @@ public:
   /**
    * read a list from the provided parameter name
    * @param name the name of the parameter to be read
-   * @param out the output of 'double' type
+   * @param out the output of 'double' type.
    * @return AWS_ERR_OK if read was successful, AWS_ERR_NOT_FOUND if the parameter was not found
+   * @note if the return code is not AWS_ERR_OK, out remains unchanged.
    */
-  virtual AwsError ReadList(const char * name, std::vector<std::string> & out) const = 0;
+  virtual AwsError ReadList(const ParameterPath & parameter_path, std::vector<std::string> & out) const = 0;
 
   /**
    * read a double value from the provided parameter name
-   * @param name the name of the parameter to be read
+   * @param parameter_path an object representing the path of the parameter to be read
    * @param out the output of 'double' type
    * @return AWS_ERR_OK if read was successful, AWS_ERR_NOT_FOUND if the parameter was not found
+   * @note if the return code is not AWS_ERR_OK, out remains unchanged.
    */
-  virtual AwsError ReadDouble(const char * name, double & out) const = 0;
+  virtual AwsError ReadDouble(const ParameterPath & parameter_path, double & out) const = 0;
 
   /**
    * read an integer value from the provided parameter name
-   * @param name the name of the parameter to be read
+   * @param parameter_path an object representing the path of the parameter to be read
    * @param out the output of 'int' type
    * @return AWS_ERR_OK if read was successful, AWS_ERR_NOT_FOUND if the parameter was not found
+   * @note if the return code is not AWS_ERR_OK, out remains unchanged.
    */
-  virtual AwsError ReadInt(const char * name, int & out) const = 0;
+  virtual AwsError ReadInt(const ParameterPath & parameter_path, int & out) const = 0;
 
   /**
    * read a boolean value from the provided parameter name
-   * @param name the name of the parameter to be read
+   * @param parameter_path an object representing the path of the parameter to be read
    * @param out the output of 'bool' type
    * @return AWS_ERR_OK if read was successful, AWS_ERR_NOT_FOUND if the parameter was not found
+   * @note if the return code is not AWS_ERR_OK, out remains unchanged.
    */
-  virtual AwsError ReadBool(const char * name, bool & out) const = 0;
+  virtual AwsError ReadBool(const ParameterPath & parameter_path, bool & out) const = 0;
 
   /**
    * read a string value from the provided parameter name
-   * @param name the name of the parameter to be read
+   * @param parameter_path an object representing the path of the parameter to be read
    * @param out the output of 'Aws::String' type
    * @return AWS_ERR_OK if read was successful, AWS_ERR_NOT_FOUND if the parameter was not found
+   * @note if the return code is not AWS_ERR_OK, out remains unchanged.
    */
-  virtual AwsError ReadString(const char * name, Aws::String & out) const = 0;
+  virtual AwsError ReadString(const ParameterPath & parameter_path, Aws::String & out) const = 0;
 
   /**
    * read a string from the provided parameter name
-   * @param name the name of the parameter to be read
+   * @param parameter_path an object representing the path of the parameter to be read
    * @param out the output of 'std::string' type
    * @return AWS_ERR_OK if read was successful, AWS_ERR_NOT_FOUND if the parameter was not found
+   * @note if the return code is not AWS_ERR_OK, out remains unchanged.
    */
-  virtual AwsError ReadStdString(const char * name, std::string & out) const = 0;
+  virtual AwsError ReadStdString(const ParameterPath & parameter_path, std::string & out) const = 0;
 
   /**
    * read a map from the provided parameter name
-   * @param name the name of the parameter to be read
+   * @param parameter_path an object representing the path of the parameter to be read
    * @param out the output of 'std::map' type
    * @return AWS_ERR_OK if read was successful, AWS_ERR_NOT_FOUND if the parameter was not found
+   * @note if the return code is not AWS_ERR_OK, out remains unchanged.
    */
-  virtual AwsError ReadMap(const char * name, std::map<std::string, std::string> & out) const = 0;
+  virtual AwsError ReadMap(const ParameterPath & parameter_path, std::map<std::string, std::string> & out) const = 0;
 };
 
 }  // namespace Client
